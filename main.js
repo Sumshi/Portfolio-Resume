d6bae883c26a2e4bb2bbf54fbd66f83c9967d775
Sumshi <sumayasomow@gmail.com>
2023-12-16 17:17:02 +0000

    menu icon not responsive

---
+++ main.js
@@ -0,0 +1,26 @@
+// select all html elements with type section
+let sections = document.querySelectorAll('section');
+
+// select all html elements with type a
+let navLinks = document.querySelector('header nav a');
+
+// add event listener to all navLinks
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /* stciky navbar */
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
